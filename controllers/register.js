2367ef46cc8a077b8612c0dc6cf42a3359aa5fa4
Martin V <martinvillarreal90@gmail.com>
2021-11-02 00:24:27 +0100

    testing

--- controllers/register.js
+++ controllers/register.js
@@ -24,7 +24,8 @@ const handleRegister = (req, res, db, bcrypt) => {
 		.then(trx.commit)
 		.catch(trx.rollback);
 	})
-	.catch(err => res.status(400).json('unable to register'));
+	//.catch(err => res.status(400).json('unable to register'));
+	.catch(err => res.json(err));
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  client: 'pg'
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
 });
 
 const app = express();
@@ -23,5 +27,4 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
 app.put('/image', (req, res) => image.handleImage(req, res, db));
 app.post('/imageUrl', (req, res) => image.handleApiCall(req, res));
 
-console.log("URL!!!", process.env.DATABASE_URL);
 app.listen(process.env.PORT || 3000, () => console.log(`app is running on port ${process.env.PORT}`));
